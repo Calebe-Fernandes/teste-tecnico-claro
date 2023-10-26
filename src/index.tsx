0614e03ccf48a113550ff57c93d53b441eca7914
Calebe-Fernandes <calebefernandesps4@gmail.com>
2023-10-26 00:29:51 -0300

    mobile modal responsive

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './colors.scss';
+import './theme/colors.scss';
 import './index.scss';
 import eng from './text/eng';
 import App from './App';
